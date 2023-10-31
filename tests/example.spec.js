53022e98fc9c3f2d51f2588e72536014280d72a7
rstan <rostykbest@gmail.com>
2023-10-31 13:29:40 +0200

    initial_config_playwrigt

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -18,11 +18,20 @@ test.describe("Test", () => {
     //
   });
 
-  test("has title", async ({ page }) => {
+  test("has title1", async ({ page }) => {
+    await test.step("Navigate to Playwrite site", async () => {
+      await page.goto("https://playwright.dev/");
+    });
+
+    await test.step("check page title", async ({ page }) => {
+      await expect(page).toHaveTitle(/Playwright/);
+    });
+  });
+
+  test("has title2", async ({ page }) => {
     await page.goto("https://playwright.dev/");
 
-    // Expect a title "to contain" a substring.
-    await expect(page).toHaveTitle(/Playwright/);
+    await expect(page).toHaveTitle(/'Hello'/);
   });
 
   test("get started link", async ({ page }) => {
