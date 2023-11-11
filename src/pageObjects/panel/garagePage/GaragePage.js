ec415bae26a83a56b589fc84240f7b6604e65606
rstan <rostykbest@gmail.com>
2023-11-11 11:58:49 +0200

    pom-1-edit

---
+++ src/pageObjects/panel/garagePage/GaragePage.js
@@ -0,0 +1,13 @@
+import BasePage from "../../BasePage.js";
+
+export default class GaragePage extends BasePage {
+  constructor(page) {
+    super(
+      page,
+      "/panel/garage",
+      page.locator("app-panel-layout", {
+        has: page.locator("button", { hasText: "Add car" }),
+      })
+    );
+  }
+}
