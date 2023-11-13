f264c6bf34eabe909a0fd2b8d078238bd4168539
rstan <rostykbest@gmail.com>
2023-11-13 14:36:35 +0200

    HomeTask_18_CLI&Env_var

--- config/smoke.js
+++ config/smoke.js
@@ -1,28 +1,22 @@
-// @ts-check
-//const { defineConfig, devices } = require("@playwright/test");
 import { defineConfig, devices } from "@playwright/test";
 import { config as testConfig } from "./config/config.js";
-
-import dotenv from "dotenv";
-
-dotenv.config();
 /**
  * Read environment variables from file.
  * https://github.com/motdotla/dotenv
  */
-//require('dotenv').config();
+// require('dotenv').config();
 
 /**
  * @see https://playwright.dev/docs/test-configuration
- *
  */
 
 const config = defineConfig({
-  testDir: "./tests",
-
+  //testDir: "./tests",
+  testMatch: "tests/**/*.spec.js",
   globalSetup: "./globalSetup",
   globalTeardown: "./globalTeardown",
-
+  grep: /@smoke/,
+  timeout: 360_000,
   /* Run tests in files in parallel */
   fullyParallel: false,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
@@ -30,43 +24,46 @@ const config = defineConfig({
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
+  workers: 2,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [["html", { open: "never" }]],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
-    slowMo: 300,
-    // httpCredentials: {
-    //   username: process.env.HTTP_CREDENTIALS_USERNAME ?? "",
-    //   password: process.env.HTTP_CREDENTIALS_PASSWORD ?? "",
-    // },
+    headless: false,
     httpCredentials: testConfig.httpCredentials,
-    launchOptions: {
-      headless: false, // Set to false for non-headless mode
-    },
-
     /* Base URL to use in actions like `await page.goto('/')`. */
-    //baseURL: process.env.BASE_URL,
     baseURL: testConfig.baseURL,
+    viewport: {
+      width: 1200,
+      height: 840,
+    },
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: "on-first-retry",
+    launchOptions: {
+      // slowMo: 1000
+    },
   },
 
   /* Configure projects for major browsers */
   projects: [
     {
+      name: "setup",
+      testMatch: "**/setup/**/*.setup.js",
+    },
+    {
       name: "chromium",
       use: { ...devices["Desktop Chrome"] },
+      dependencies: ["setup"],
     },
-
+    //
     // {
-    //   name: "firefox",
-    //   use: { ...devices["Desktop Firefox"] },
+    //   name: 'firefox',
+    //   use: { ...devices['Desktop Firefox'] },
     // },
-
+    //
     // {
-    //   name: "webkit",
-    //   use: { ...devices["Desktop Safari"] },
+    //   name: 'webkit',
+    //   use: { ...devices['Desktop Safari'] },
     // },
 
     /* Test against mobile viewports. */
